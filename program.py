58d70956ac99d30af7eb4de45d43182c3cb36dcf
OhMyGodness123 <46843769+OhMyGodness123@users.noreply.github.com>
2019-11-13 16:37:52 +0300

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git project')
     print('and i change it every day')
     print('new commit from pycharm')
+    print('GitHub commit')
 
 
 if __name__ == '__main__':
